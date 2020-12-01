5c3169b8a5f75e7dcc475e4943092a1529d53051
ajoeviola <48599775+ajoeviola@users.noreply.github.com>
2020-12-01 16:26:12 -0500

    Commiting initial outline of project

---
+++ Source.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "person.h"
+using namespace std;
+
+/*
+
+This program will assess an individual's risk of contracting covid in terms of low/moderate/high risk with a short assesment.
+It will also store each person who has their risk calculated in a dynamically allocated array, which can be displayed by the user.
+
+
+*/
+
+
+int main() 
+{
+
+
+
+
+
+
+
+}
---
+++ person.h
@@ -0,0 +1,76 @@
+#ifndef PERSON_H
+#define PERSON_H
+#include <iostream>
+using namespace std;
+
+/*
+
+Class Person will contain the following: 
+	private members: string name, int riskLevel, int age, string riskStatus
+	public member functions: getRiskLevel(), getName(), getAge(), getRiskStatus(), setRiskLevel(), setName(), setAge(), getRiskStatus(), print(), assess() (function assess will contain the questions for assesment and calculate risk level based on each question)
+	a default constructor as well as another constructor that takes in the users name as a parameter
+	a destructor that deletes the class object called 
+*/
+class Person {
+private:
+
+public:
+
+
+};
+
+
+
+/*
+
+class personList will contain the following:
+	private members: int size, int numOfPeople, Person* people
+	public member functions: addPerson(), print(), 
+	default constructor and constructor that sets size to 0
+	destructor that deletes the class object
+*/
+class personList {
+private:
+	
+public:
+	
+};
+
+
+/* 
+Function printMenu will print a menu that gives the user an option of either:
+A. Assessing a new person's covid risk level
+B. Display the current list (personList) of people already assesed 
+C. Exit Program
+
+*/
+void printMenu() {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
